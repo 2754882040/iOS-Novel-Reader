37ba04a28828384c7c5434de28e9950823db33e4
liming <yliming163@gmail.com>
2021-07-30 14:17:03 -0400

    statu check

--- Libbris/LibbrisApp.swift
+++ Libbris/LibbrisApp.swift
@@ -7,11 +7,35 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("log-didFinishLaunching")
+        return true
+    }
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        print("log-DidReceiveMemoryWarning")
+    }
+}
+
 @main
 struct LibbrisApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.onChange(of: scenePhase) { newScenePhase in
+            switch newScenePhase {
+            case .active:
+              print("APP active")
+            case .inactive:
+              print("App inactive")
+            case .background:
+              print("App background")
+                
+            @unknown default:
+              print("default")
+            }
         }
     }
 }
