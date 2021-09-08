28d7582e514599c1f05fc78fd5fc6d4b77794227
Maureen <maorouwen@163.com>
2021-09-08 15:31:34 -0400

    add more button file

--- Libbris/BookListVertical.swift
+++ Libbris/BookListVertical.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct BookListVertical: View {
     @State var stringLiteral: String = localizedString(text: strToRead);
+    
     var body: some View {
         Text(stringLiteral).padding(.leading)
             .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("switchLanguage")), perform: { _ in
                                                             self.stringLiteral = localizedString(text: strToRead)})
         ScrollView(.vertical){
             VStack(alignment:.leading, spacing: 50){
-                ForEach(0..<7) {_ in
+                ForEach(0..<3) {_ in
                     HorizontalBookList();
+                }
+                
+                Button(action:{} ){
+                    Text("More")
                 }
             }
         }
